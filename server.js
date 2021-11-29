54f90df55abead47352091ae4aa580e0f0be2de0
brandonswansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2021-11-29 10:07:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ require("dotenv").config();
 // pull PORT from .env, give default value of 3001
 // pull DATABASE_URL from .env
 // const port = process.env.PORT || 3001;
-======
-const { PORT = 5000, DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 const port = process.env.PORT || 5000;
 // import express
 const express = require("express");
